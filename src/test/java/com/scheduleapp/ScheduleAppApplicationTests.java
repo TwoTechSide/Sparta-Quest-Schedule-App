d0cbc8cdcc9e4ce568b23d06c61743cb53abca09
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-01 14:11:25 +0900

    feat: add test data for DB

--- src/test/java/com/scheduleapp/ScheduleAppApplicationTests.java
+++ src/test/java/com/scheduleapp/ScheduleAppApplicationTests.java
@@ -1,13 +1,28 @@
 package com.scheduleapp;
 
+import com.scheduleapp.entity.ScheduleEntity;
+import com.scheduleapp.service.ScheduleService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ScheduleAppApplicationTests {
 
+    @Autowired
+    private ScheduleService scheduleService;
+
     @Test
     void contextLoads() {
+
+        ScheduleEntity scheduleEntity = ScheduleEntity.builder()
+                .title("abcd")
+                .content("hi")
+                .userName("2TS")
+                .password("1234")
+                .build();
+
+        scheduleService.save(scheduleEntity);
     }
 
 }
