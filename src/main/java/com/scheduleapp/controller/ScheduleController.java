817330becab58d6cc648c48ce072acb00700d265
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-01 14:50:40 +0900

    #3 refactor: change return type of createSchedule to ScheduleDto

--- src/main/java/com/scheduleapp/controller/ScheduleController.java
+++ src/main/java/com/scheduleapp/controller/ScheduleController.java
@@ -1,5 +1,6 @@
 package com.scheduleapp.controller;
 
+import com.scheduleapp.dto.ScheduleDto;
 import com.scheduleapp.entity.Schedule;
 import com.scheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,8 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping("/schedules")
-    public Schedule createSchedule(@RequestBody Schedule schedule) {
-        return scheduleService.saveSchedule(schedule);
+    public ScheduleDto createSchedule(@RequestBody Schedule schedule) {
+        Schedule savedSchedule = scheduleService.saveSchedule(schedule);
+        return scheduleService.entityToDto(savedSchedule);
     }
 }
