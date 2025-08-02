6a80acd437ca4084519f1831bcc36b3e7981c8b4
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-02 20:32:53 +0900

    #9 chore: add comments to ScheduleController

--- src/main/java/com/scheduleapp/controller/ScheduleController.java
+++ src/main/java/com/scheduleapp/controller/ScheduleController.java
@@ -20,20 +20,23 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    // 새로운 일정 추가
     @PostMapping
     public ResponseEntity<ScheduleDto> createSchedule(@RequestBody Schedule schedule) {
         Schedule savedSchedule = scheduleService.saveSchedule(schedule);
         return new ResponseEntity<>(scheduleService.entityToDto(savedSchedule), HttpStatus.CREATED);
     }
 
+    // userName이 있을 경우 필터링을 거치고 아닌 경우 모든 일정 반환
     @GetMapping
     public ResponseEntity<List<ScheduleDto>> findAllSchedules(@RequestParam(required = false) String userName) {
         return new ResponseEntity<>(scheduleService.findAllSchedule(userName), HttpStatus.OK);
     }
 
+    // Schedule ID로 특정되는 일정 반환
+    // userId 일정을 찾을 수 없는 경우 NOT FOUND 반환
     @GetMapping("/{scheduleId}")
     public ResponseEntity<ScheduleDto> findSchedules(@PathVariable Long scheduleId) {
-        // userId 일정을 찾을 수 없는 경우 NOT FOUND 반환
         try {
             ScheduleDto foundScheduleDto = scheduleService.findScheduleById(scheduleId);
             return new ResponseEntity<>(foundScheduleDto, HttpStatus.OK);
@@ -42,6 +45,8 @@ public class ScheduleController {
         }
     }
 
+    // Schedule의 Title, Username 수정
+    // Schedule의 id를 찾을 수 없으면 BAD_REQUEST, 비밀번호가 일치하지 않으면 UNAUTHORIZED로 예외 처리
     @PatchMapping("/{scheduleId}")
     public ResponseEntity<ScheduleDto> editSchedule(@PathVariable Long scheduleId, @RequestBody EditScheduleTitleAndUsernameDto editScheduleTitleAndUsernameDto) {
         try {
