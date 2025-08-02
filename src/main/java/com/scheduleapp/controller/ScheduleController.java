793581f51560ffd4b1f49a5efffeb82664d95241
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-02 14:16:45 +0900

    #9 refactor: return ResponseEntity instead of DTO in controller

--- src/main/java/com/scheduleapp/controller/ScheduleController.java
+++ src/main/java/com/scheduleapp/controller/ScheduleController.java
@@ -19,14 +19,14 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping
-    public ScheduleDto createSchedule(@RequestBody Schedule schedule) {
+    public ResponseEntity<ScheduleDto> createSchedule(@RequestBody Schedule schedule) {
         Schedule savedSchedule = scheduleService.saveSchedule(schedule);
-        return scheduleService.entityToDto(savedSchedule);
+        return new ResponseEntity<>(scheduleService.entityToDto(savedSchedule), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<ScheduleDto> findAllSchedules(@RequestParam(required = false) String userName) {
-        return scheduleService.findAllSchedule(userName);
+    public ResponseEntity<List<ScheduleDto>> findAllSchedules(@RequestParam(required = false) String userName) {
+        return new ResponseEntity<>(scheduleService.findAllSchedule(userName), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
